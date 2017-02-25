7986baf1d1d20fc1082b5953d726337cac19322a
takahashim <maki@rubycolor.org>
2017-02-25 17:56:08 +0900

    initial commit

---
+++ lib/epubcheck/ruby.rb
@@ -0,0 +1,7 @@
+require "epubcheck/ruby/version"
+
+module Epubcheck
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/epubcheck/ruby/version.rb
@@ -0,0 +1,5 @@
+module Epubcheck
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
