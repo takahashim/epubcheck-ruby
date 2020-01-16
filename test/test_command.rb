1f6cbb516e65cb316215ee2fa8508d526a590f70
Marat Radchenko <marat@slonopotamus.org>
2020-01-16 20:15:43 +0300

    Add test that successfully validates epub file
    childrens-literature.epub was taken from https://github.com/IDPF/epub3-samples

--- test/test_command.rb
+++ test/test_command.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class TestUnitColor < Test::Unit::TestCase
-  test "Exit with error status when missing argument" do
-    system(File.expand_path('../exe/epubcheck', __dir__))
-    assert_equal false, $?.success?
+  test "Exit with status 1 when missing argument" do
+    system('bundle', 'exec', 'epubcheck')
+    assert_equal 1, $?.exitstatus
+  end
+
+  test "Exit with status 0 when given a valid ebook file" do
+    system('bundle', 'exec', 'epubcheck', File.join(__dir__, 'childrens-literature.epub'))
+    assert_equal 0, $?.exitstatus
   end
 end
