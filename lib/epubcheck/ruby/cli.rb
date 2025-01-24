74e7f55d56254d85a5bf4e1378e76daead31061d
takahashim <takahashimm@gmail.com>
2025-01-24 19:55:58 +0900

    update jar 5.2.0 -> 5.2.1

--- lib/epubcheck/ruby/cli.rb
+++ lib/epubcheck/ruby/cli.rb
@@ -3,7 +3,7 @@ require 'epubcheck/ruby'
 module Epubcheck
   module Ruby
     class CLI
-      JAR_FILE = File.join(Epubcheck::Ruby::JAR_DIR, 'epubcheck-5.2.0/epubcheck.jar')
+      JAR_FILE = File.join(Epubcheck::Ruby::JAR_DIR, 'epubcheck-5.2.1/epubcheck.jar')
 
       def initialize
       end
